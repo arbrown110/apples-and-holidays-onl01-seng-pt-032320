954a35c6011faca44ea7d6f7325dfba5b574cfa8
chic-caller-7520 <arbrown110@gmail.com>
2020-04-04 03:48:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -22,7 +22,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
   
 end
 
-ddef second_supply_for_fourth_of_july(holiday_hash)
+def second_supply_for_fourth_of_july(holiday_hash)
   holiday_hash[:summer][:fourth_of_july][1]
 end
 
